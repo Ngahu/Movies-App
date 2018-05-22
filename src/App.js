11c9a19104e9b1c51a3af1e679f9e47f16d66bc3
Ngahu <jamaalaraheem@gmail.com>
2018-05-23 00:09:12 +0300

    creating of the apps navigation bar and also the logo section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './image/green_app_icon.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        homepage
+        
+        <table className="titleBar">
+          <tbody>
+            <tr>
+
+              <td>
+                <img width="50" src={logo} alt="logo" />
+              </td>
+              <td width="8"/>
+              <td>
+                <h3>MovieDB Search</h3>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <input placeholder="Search for a movie,tv show ,person..." className="myInput"/>
       </div>
     );
   }
