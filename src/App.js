3d280d15a91de4f4f92e08dc8c02b427326cc2ae
Ngahu <jamaalaraheem@gmail.com>
2018-05-23 17:59:49 +0300

    being able to change the state from the old state and switch to the new state

--- src/App.js
+++ src/App.js
@@ -6,13 +6,28 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props)
-    // this.state = ({})
+
+    const movies =[
+      {id:0,title:'Avengers:infinity war',overview:'very amazing'},
+      {id:1,title:'Avengersfkfkfk:ffffinfinity war',overview:'very amazing'},
+      {id:2,title:'Avengersqqwww:dskkskdinfinity war',overview:'very amazing'},
+      {id:3,title:'Avengersgggg:ggggggggggginfinity war',overview:'very amazing'}
+    ] 
     
     this.state = {rows: [
       <p key="0">this is  my row0</p>,
       <p key="1">this is  my row1</p>,
       <p key="2">this is  my row2</p>
     ]}
+
+    let movieRows =[]
+    movies.forEach((movie)  =>{
+      console.log(movie.id)
+      movieRows.push(<p key={movie.id}><strong>movie title</strong>:{movie.title}</p>)
+    })
+
+    // change the state from old to a new state
+    this.state = {rows:movieRows}
   }
   render() {
     return (
